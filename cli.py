f4cfd1e43487f5a7a58a1307149f1e3e83a2756e
Bertrand Atangana Ekobo <129292669+bertrand12345@users.noreply.github.com>
2024-09-08 12:53:31 +0000

    adding the final command-line tool

--- cli.py
+++ cli.py
@@ -4,20 +4,16 @@ from mlib.mchange import change
 
 
 @click.command()
-@click.option(
-    "--amount",
-    prompt = "Amount: ",
-    help = " This is my change calculation"
-)
-
+@click.option("--amount", prompt="Amount: ", help=" This is my change calculation")
 def make_change(amount):
     "Give correct change in coins"
 
     result = change(float(amount))
-    click.echo(click.style(f"change for {amount}: ", fg = "red"))
+    click.echo(click.style(f"change for {amount}: ", fg="red"))
     for correct_change in result:
         for num, coin in correct_change.items():
-            click.echo(click.style(f"{coin}: {num}", fg = "green"))
+            click.echo(click.style(f"{coin}: {num}", fg="green"))
+
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
