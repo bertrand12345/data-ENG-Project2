4817835e9b22adcff0a3f130868ffe61f6c2c769
Bertrand Atangana Ekobo <129292669+bertrand12345@users.noreply.github.com>
2024-09-08 11:28:40 +0000

    adding cli

--- cli.py
+++ cli.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 import click
+from mlib.mchange import change
+
+
+@click.command()
+@click.option(
+    "--amount",
+    prompt = "Amount: ",
+    help = " This is my change calculation"
+)
+
+def make_change(amount):
+    "Give correct change in coins"
+
+    result = change(float(amount))
+    click.echo(click.style(f"change for {amount}: ", fg = "red"))
+    for correct_change in result:
+        for num, coin in correct_change.items():
+            click.echo(click.style(f"{coin}: {num}", fg = "green"))
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    make_change()
