7e0815d499d4bf9e8c93923cd36662ed27278d60
Bertrand Atangana Ekobo <129292669+bertrand12345@users.noreply.github.com>
2024-09-09 15:57:18 +0000

    adding new wikiphrases

--- wikiphrases.py
+++ wikiphrases.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-# from corenlp import get_phrases
-# import fire
+from nlplogic.corenlp import get_phrases
+import fire
 
 
-# if __name__ == "__main__":
-# fire.Fire(get_phrases)
+if __name__ == "__main__":
+    fire.Fire(get_phrases)
+ 
